a05fa77f480be7aa21d84029895355f5a33391ce
Ievgen Dimov <ievgen7@gmail.com>
2021-11-07 14:56:04 -0800

    perfecting the scroll function

--- app.js
+++ app.js
@@ -58,6 +58,38 @@ window.addEventListener('scroll', function (){
   }
 })
 
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach(function(link){
+  link.addEventListener('click', function (e) {
+    // prevents the default settings from being executed
+    e.preventDefault();
+    //  navigate to a specific spot
+
+    const id = e.currentTarget.getAttribute('href').slice(1);
+    const element = document.getElementById(id);
+
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height
+
+    const fixedNav = navbar.classList.contains('fixed-nav');
+    let position = element.offsetTop - navHeight;
+
+    if(!fixedNav){
+      position = position - navHeight;
+    }
+    if(navHeight > 82){
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left:0, 
+      top: position,
+    });
+    linksContainer.style.height = 0;
+  });
+})
 
 // ********** smooth scroll ************
 // select links
